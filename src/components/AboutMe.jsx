0ae9e317e46c18aa851bb5c47c648d04e255138d
kevinh81760 <kevinh81760@gmail.com>
2025-04-12 17:36:05 -0700

    more animations

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,26 +5,28 @@ import FadeInOnScroll from '../components/FadeInOnScroll'
 function AboutMe() {
     return (
         <div>
+
             <div className="relative bottom-75">
                 <FadeInOnScroll>
-                    <h1 className="text-[34px] font-bold ml-20">About me</h1>
+                    <div>
+                        <h1 className="text-[34px] font-bold ml-20">
+                            About me
+                        </h1>
+
+                        <p className="text-[30px] ml-20 mt-8">
+                            I'm Kevin Ha, a third-year <br />
+                            Computer Science student at <br />
+                            SJSU. I specialize in full-stack <br />
+                            development, AI agents, and <br />
+                            scalable systems. Proficient using <br />
+                            MERN, Java, and Python. Outside <br />
+                            of coding, I love photography and <br />
+                            staying active in the gym.
+                        </p>
+                    </div>
                 </FadeInOnScroll>
             </div>
 
-            <div className="relative bottom-65">
-                <FadeInOnScroll>
-                    <p className="text-[30px] ml-20">
-                        I'm Kevin Ha, a third-year <br />
-                        Computer Science student at <br />
-                        SJSU. I specialize in full-stack <br />
-                        development, AI agents, and <br />
-                        scalable systems. Proficient using <br />
-                        MERN, Java, and Python. Outside <br />
-                        of coding, I love photography and <br />
-                        staying active in the gym.
-                    </p>
-                </FadeInOnScroll>
-            </div>
 
             <div className="relative bottom-170 left-220 justify-end">
                 <FadeInOnScroll>
